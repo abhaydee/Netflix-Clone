f2474d91fee7f4c6783fe2ca93b0f049f08b955f
abhay <abhaygirish1997@gmail.com>
2020-07-19 00:08:14 +0530

    Added data.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react'
 import Row from './Row'
 import requests from './requests'
+import {dataRequired} from './data'
 export default function App() {
     return (
         <div className="App">
-            <Row title="NETFLIX ORIGINALS" fetchUrl={requests.fetchNetflixOriginals}/>
-            <Row title="Trending Now" fetchUrl={requests.fetchTrending}/>
+            {/* <Row title="NETFLIX ORIGINALS" fetchUrl={requests.fetchNetflixOriginals}/>
+            <Row title="Trending Now" fetchUrl={requests.fetchTrending}/> */}
+            {dataRequired.map((rowData)=>(
+                <Row title={rowData.title} fetchUrl={rowData.url}/>
+            ))}
         </div>
     )
 }
