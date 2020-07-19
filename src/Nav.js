e3211e352f6850174bc05dd75d7dff77d8a05434
abhay <abhaygirish1997@gmail.com>
2020-07-19 14:12:20 +0530

    fixed navbar styles

--- src/Nav.js
+++ src/Nav.js
@@ -1,15 +1,21 @@
 import React, { useEffect, useState } from "react";
 import "./nav.css";
 function Nav() {
-  cosnt[(show, handleShow)] = useState("false");
+  const [show, handleShow] = useState(false);
   useEffect(() => {
     window.addEventListener("scroll", () => {
       if (window.scrollY > 100) {
+        handleShow(true);
+      } else {
+        handleShow(false);
       }
     });
-  });
+    return () => {
+      window.removeEventListener("scroll");
+    };
+  }, []);
   return (
-    <div className="navbar">
+    <div className={`navbar ${show && "nav__black"}`}>
       <img
         className="nav__logo"
         src="https://assets.brand.microsites.netflix.io/assets/1ed15bca-b389-11e7-9274-06c476b5c346_cm_800w.png?v=45"
