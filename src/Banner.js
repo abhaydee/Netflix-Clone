291117907d473e9a648938eec843441475c94078
abhay <abhaygirish1997@gmail.com>
2020-07-19 12:47:29 +0530

    background image for banner

--- src/Banner.js
+++ src/Banner.js
@@ -7,14 +7,29 @@ function Banner() {
         async function fetchData(){
             const request=await instance.get(dataRequired[0].url)
             console.log("the banner api request",request)
-            setMovies(request.data.results)
+            setMovies(request.data.results[Math.floor(Math.random()* request.data.results.length -1)]);
         }
         fetchData();
-    },[dataRequired[0].url])
+    },[dataRequired[0].url]);
+    console.log("the movie ",movie)
     return (
-        <header>
-            <h1> I am the Banner</h1>
-        </header>
+        <div className="banner"
+            style={{
+                backgroundSize:"cover",
+                backgroundImage:`url("https://image.tmdb.org/t/p/original/${movie.backdrop_path && movie.backdrop_path}")`,
+                backgroundPosition:"center center"
+            }}
+        >
+            <div className="banner__contents">
+                <h1>
+                    {movie?.title || movie?.name || movie?.original_name}
+                </h1>
+                <div className="banner__buttons">
+                     <button className="banner__button">Play</button>
+                     <button className="banner__button">My List</button>
+                </div>
+            </div>
+        </div>
     )
 }
 
