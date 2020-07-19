372be620cd84b85c30ea3fe126cf4fa3e4fd96fb
abhay <abhaygirish1997@gmail.com>
2020-07-19 13:18:29 +0530

    UI Changes to Banner

--- src/Banner.js
+++ src/Banner.js
@@ -1,6 +1,7 @@
 import React,{useState,useEffect} from 'react'
 import {instance} from './axios';
 import {dataRequired} from './data'
+import './banner.css'
 function Banner() {
     const [movie,setMovies]=useState([]);
     useEffect(()=>{
@@ -11,7 +12,11 @@ function Banner() {
         }
         fetchData();
     },[dataRequired[0].url]);
-    console.log("the movie ",movie)
+    console.log("the movie ",movie);
+
+    function truncate(str,n){
+        return str.length>n ?str.substr(0,n-1) + "...":str;
+    }
     return (
         <div className="banner"
             style={{
@@ -21,13 +26,16 @@ function Banner() {
             }}
         >
             <div className="banner__contents">
-                <h1>
+                <h1 className="banner__title">
                     {movie?.title || movie?.name || movie?.original_name}
                 </h1>
                 <div className="banner__buttons">
                      <button className="banner__button">Play</button>
                      <button className="banner__button">My List</button>
                 </div>
+                <h1 className="banner__description">
+                    {movie.overview && truncate(movie.overview,150)}
+                </h1>
             </div>
         </div>
     )
